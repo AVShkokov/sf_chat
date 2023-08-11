698742df9adc95b5006c4eb9cebf1fbc6b25bd3e
Anton Shkokov [Tony] <A.V.Shkokov@mail.ru>
2023-08-11 21:54:13 +0300

    Refactoring
    - Code formate
    - Added db.txt file with queries for MySQL database creation

--- chat.h
+++ chat.h
@@ -8,37 +8,37 @@
 #include <vector>
 
 class Chat {
- public:
-  Chat() = default;
-  explicit Chat(const std::string& chat_name) : m_chat_name(chat_name) {}
+public:
+	Chat() = default;
+	explicit Chat(const std::string& chat_name) : m_chat_name(chat_name) {}
 
-  ~Chat() = default;
+	~Chat() = default;
 
-  const std::string& GetChatName() const;
+	const std::string& GetChatName() const;
 
-  void Init();
+	void Init();
 
- private:
-  std::string m_chat_name;
+private:
+	std::string m_chat_name;
 
-  User m_user;
-  std::vector<User> m_users;
-  std::vector<Message> m_messages;
+	User m_user;
+	std::vector<User> m_users;
+	std::vector<Message> m_messages;
 
-  DataBase m_database;
-  
-  void readFromDB(const bool& isUsers);
+	DataBase m_database;
 
-  void mainMenu();
+	void readFromDB(const bool& isUsers);
 
-  const bool singUp();
-  const bool singIn();
+	void mainMenu();
 
-  void loadHistory(const std::string& user_name);
+	const bool singUp();
+	const bool singIn();
 
-  void createChat();
+	void loadHistory(const std::string& user_name);
 
-  const bool privateMessage(std::string& text, std::string& to);
+	void createChat();
 
-  const bool chatCommand(const std::string& command) const;
+	const bool privateMessage(std::string& text, std::string& to);
+
+	const bool chatCommand(const std::string& command) const;
 };
--- message.h
+++ message.h
@@ -3,22 +3,22 @@
 #include <string>
 
 class Message {
- public:
-  Message() = default;
-  Message(const std::string& from, const std::string& to,
-          const std::string& text)
-      : m_from(from), m_to(to), m_text(text) {}
+public:
+	Message() = default;
+	Message(const std::string& from, const std::string& to,
+		const std::string& text)
+		: m_from(from), m_to(to), m_text(text) {}
 
-  ~Message() = default;
+	~Message() = default;
 
-  const std::string& GetFrom() const;
+	const std::string& GetFrom() const;
 
-  const std::string& GetTo() const;
+	const std::string& GetTo() const;
 
-  const std::string& GetText() const;
+	const std::string& GetText() const;
 
- private:
-  std::string m_from;
-  std::string m_to;
-  std::string m_text;
+private:
+	std::string m_from;
+	std::string m_to;
+	std::string m_text;
 };
